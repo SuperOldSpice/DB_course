d6398372ec554e9d27324bbe8a3c4969bc63334b
SuperOldSpice <felloso666@gmail.com>
2021-12-03 05:31:37 +0200

    Finished LAB5.sql

--- LAB4.sql
+++ LAB4.sql
@@ -1,4 +1,4 @@
-use IS01_20_TS
+﻿use IS01_20_TS
 
 /*TASK1*/
 SELECT DT, Sum(PaymentOrders.Sum) AS Total FROM PaymentOrders
@@ -7,7 +7,7 @@ GROUP BY DT
 /*TASK2*/
 SELECT Reason as reason, Sum(PaymentOrders.Sum) AS Total FROM PaymentOrders
 GROUP BY Reason
-HAVING Sum(PaymentOrders.Sum) > 400000;
+HAVING Sum(PaymentOrders.CoECsID) >= 3;
 
 /*TASK3*/
 SElECT * INTO CheapOrders
@@ -27,3 +27,11 @@ INSERT INTO CheapOrders
 SELECT Number, Reason, Recepient, Sum, 
 DT, CoECsID FROM PaymentOrders
 WHERE Sum < 400000
+
+/*ADDING ROWS TO PAYMENTORDERS*/
+
+INSERT INTO PaymentOrders(Number, Reason, Recepient, Sum, DT, CoECsID) VALUES
+(1321, 'Встановлення рядів', 'ВПН', 300000, '2021-05-12', 1),
+(5213, 'Опалення музею', 'ЖЕК', 250000, '2021-08-27', 6),
+(2164, 'Прибирання територій', 'РКВ', 40000, '2021-05-23', 7),
+(4212, 'Підвищення кваліфікації співробітників', 'МПК', 300000, '2021-02-23', 4);
